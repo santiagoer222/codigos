718ce1e4da5419e122a6447a8bfe0be7da87d290
santiagoer222 <133126093+santiagoer222@users.noreply.github.com>
2023-05-24 07:30:37 -0500

    Update codigos.m

--- codigos.m
+++ codigos.m
@@ -1,10 +1,1 @@
-#Juan_Agurto
-#quien_es_tu_idolo
-#IEM_ESCUELA_NORMAL_PASTO
-#11-1
-idolo = input("¿Quién es tu ídolo? ")
 
-if idolo.lower() == "cristiano ronaldo":
-    print("¡Eso es genial! Cristiano Ronaldo es un increíble futbolista y una inspiración para muchos.")
-else:
-    print("Ese es un gran ídolo también, ¡pero para mí, Cristiano Ronaldo es realmente asombroso!")
