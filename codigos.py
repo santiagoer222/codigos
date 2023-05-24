5f3b7950bb7197ca77e6f92460e68f47d393df03
santiagoer222 <133126093+santiagoer222@users.noreply.github.com>
2023-05-24 07:30:16 -0500

    Update codigos.py

--- codigos.py
+++ codigos.py
@@ -1,1 +1,14 @@
+#Juan_Agurto
+#quien_es_tu_idolo
+#IEM_ESCUELA_NORMAL_PASTO
+#11-1
+idolo = input("¿Quién es tu ídolo? ")
 
+if idolo.lower() == "cristiano ronaldo":
+    print("¡Eso es genial! Cristiano Ronaldo es un increíble futbolista y una inspiración para muchos.")
+else:
+    print("Ese es un gran ídolo también, ¡pero para mí, Cristiano Ronaldo es realmente asombroso!")
+#NOMBRE_DEL_DUEÑO_CODIGO
+#TEMATICA_DEL_CODIGO
+#IEM_ESCUELA_NORMAL_PASTO
+#CURSO
